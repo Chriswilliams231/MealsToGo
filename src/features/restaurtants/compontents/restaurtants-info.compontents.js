6a58daa1f42441182223b0aca45905b824dfe85a
Chriswilliams231 <70972542+Chriswilliams231@users.noreply.github.com>
2022-01-08 17:08:24 -0800

    added new components

--- src/features/restaurtants/compontents/restaurtants-info.compontents.js
+++ src/features/restaurtants/compontents/restaurtants-info.compontents.js
@@ -1,11 +1,14 @@
 import React from 'react';
 //UI package
 import styled from 'styled-components/native';
-import { Text, StyleSheet, View } from 'react-native';
+import { Text, StyleSheet, View, Image } from 'react-native';
 import { Card } from 'react-native-paper';
 import { SvgXml } from 'react-native-svg';
+import { Spacer } from '../../../components/spacer/spacer.component'
 
 import star from '../../../../assets/star'
+import open from '../../../../assets/open'
+// import { Section } from 'react-native-paper/lib/typescript/components/List/List';
 
 
 //The props help you load whats in your themed folder
@@ -30,6 +33,15 @@ const Rating = styled(View)`
     padding-top: ${(props) => props.theme.space[2]};
     padding-bottom: ${(props) => props.theme.space[2]};
 `;
+const Section = styled(View)`
+    flex-direction: row;
+    align-items: center;
+`;
+const SectionEnd = styled(View)`
+    flex:1;
+    flex-direction: row;
+    justify-content: flex-end;
+`
 const Address = styled(Text)`
     font-family: ${(props) => props.theme.fonts.body};
     font-size: ${(props) => props.theme.fontSizes.caption}
@@ -38,14 +50,14 @@ const Address = styled(Text)`
 
 export const RestaurantInfo = ({ restaurant = {} }) => {
     const { name = "Something",
-        icon,
+        icon = "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
         photos = [
             "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg"
         ],
         address = '999 something street',
         isOpenNow = true,
         rating = 4,
-        isClosedTemporarily
+        isClosedTemporarily = true
     } = restaurant;
 
     const ratingArray = Array.from(new Array(Math.floor(rating)));
@@ -55,14 +67,26 @@ export const RestaurantInfo = ({ restaurant = {} }) => {
             <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
             <Info>
                 <Title>{name}</Title>
-                <Rating>
-                    {ratingArray.map(() => (
-                        <SvgXml xml={star} width={20} height={20} />
-                    ))}
-                </Rating>
+                <Section>
+                    <Rating>
+                        {ratingArray.map(() => (
+                            <SvgXml xml={star} width={20} height={20} />
+                        ))}
+                    </Rating>
+                    <SectionEnd>
+                        {isClosedTemporarily && (
+                            <Text variant="label" style={{ color: "red" }}>
+                                CLOSED TEMPORARILY
+                            </Text>
+                        )}
+                        <Spacer variant="left.large" />
+                        {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
+                        <Spacer variant="left.large" />
+                        <Image style={{ width: 15, height: 15 }} source={{ uri: icon }} />
+                    </SectionEnd>
+                </Section>
                 <Address>{address}</Address>
             </Info>
-
         </RestaurantCard>
     )
 }
