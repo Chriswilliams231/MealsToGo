f8f0f522301aa2e216f6742109174d5a1817686f
Chriswilliams231 <70972542+Chriswilliams231@users.noreply.github.com>
2022-01-10 12:23:09 -0800

    added menu tabs

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { Text } from 'react-native'
 import { ThemeProvider } from 'styled-components/native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
 
 import { useFonts as useOswald, Oswald_400Regular, } from '@expo-google-fonts/oswald';
 import { useFonts as useLato, Lato_400Regular, } from '@expo-google-fonts/lato';
@@ -29,12 +30,32 @@ export default function App() {
   if (!oswaldLoaded || !latoLoaded) {
     return null
   }
-
+  // retrieved the Icons from @expo/icons
   return (
     <>
       <ThemeProvider theme={theme}>
         <NavigationContainer>
-          <Tab.Navigator>
+          <Tab.Navigator
+            screenOptions={({ route }) => ({
+              tabBarIcon: ({ color, size }) => {
+                let iconName;
+
+                if (route.name === 'Restaurants') {
+                  iconName = "md-restaurant";
+
+                } else if (route.name === 'Settings') {
+                  iconName = "md-settings";
+                } else if (route.name === 'Map') {
+                  iconName = "md-map"
+                }
+
+                // You can return any component that you like here!
+                return <Ionicons name={iconName} size={size} color={color} />;
+              },
+              tabBarActiveTintColor: 'tomato',
+              tabBarInactiveTintColor: 'gray',
+            })}
+          >
             <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
             <Tab.Screen name="Map" component={Map} />
             <Tab.Screen name="Settings" component={Settings} />
